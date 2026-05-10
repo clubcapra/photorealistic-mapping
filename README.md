fd1a8f44059a6a6152930500a89e1f59e153ccc1
nathan-gt <nathan.gt.protic@gmail.com>
2026-05-10 23:43:30 +0000

    Stable-ish parameters

--- README.md
+++ README.md
@@ -2,7 +2,35 @@
 
 This repository is more of a test (as of writing these lines) than anything, this is figure to change.
 
-### Prerequisites: 
+## Prerequisites: 
 - Livox MID 360
 - ROS 2 humble
 - Livox SDK 2
+
+## Rosbag
+To record a bag, use:
+```bash
+rosbag record -a -O my_recording.bag
+```
+
+To replay a bag to rebuild a rtabmap, use:
+```bash
+rosbag play my_recording.bag --topics \
+  /livox/lidar \
+  /livox/lidar/deskewed \
+  /livox/imu \
+  /imu/data \
+  /imu/data_raw \
+  /imu/mag \
+  /imu/pressure \
+  /imu/temperature \
+  /scan \
+  /input_scan \
+  /input_scan/deskewed \
+  /gps/fix \
+  /fix \
+  /tf \
+  /tf_static \
+  /joint_states \
+  /robot_description
+```
--- src/vectornav_udp_bridge/launch/run.launch.py
+++ src/vectornav_udp_bridge/launch/run.launch.py
@@ -11,6 +11,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
@@ -33,10 +34,10 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'pi_ip':               LaunchConfiguration('pi_ip'),
-                'udp_port':            LaunchConfiguration('udp_port'),
+                'udp_port':            ParameterValue(LaunchConfiguration('udp_port'), value_type=int),
                 'imu_frame':           LaunchConfiguration('imu_frame'),
                 'gps_frame':           LaunchConfiguration('gps_frame'),
-                'subscribe_interval':  LaunchConfiguration('subscribe_interval'),
+                'subscribe_interval':  ParameterValue(LaunchConfiguration('subscribe_interval'), value_type=float),
             }],
         ),
     ])
